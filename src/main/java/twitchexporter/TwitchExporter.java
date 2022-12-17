ff8f6ea36d00264ca1abb26b97045fa13f35d53d
boardengineer <77598527+boardengineer@users.noreply.github.com>
2022-12-17 01:41:51 -0800

    Propagates relic info

--- src/main/java/twitchexporter/TwitchExporter.java
+++ src/main/java/twitchexporter/TwitchExporter.java
@@ -3,8 +3,10 @@ package twitchexporter;
 import basemod.BaseMod;
 import basemod.interfaces.PostInitializeSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -37,6 +39,7 @@ public class TwitchExporter implements PostInitializeSubscriber {
         httpCon.setDoOutput(true);
         httpCon.setRequestMethod("PUT");
         httpCon.setRequestProperty("Content-Type", "application/json");
+        httpCon.setRequestProperty("Accept", "application/json");
         OutputStreamWriter out = new OutputStreamWriter(
                 httpCon.getOutputStream());
 
@@ -44,15 +47,42 @@ public class TwitchExporter implements PostInitializeSubscriber {
 
         int playerHp = 0;
         int playerMaxHp = 0;
+        JsonArray relicsArray = new JsonArray();
 
-        if(AbstractDungeon.player != null) {
+        if (AbstractDungeon.player != null) {
             playerHp = AbstractDungeon.player.currentHealth;
             playerMaxHp = AbstractDungeon.player.currentHealth;
+
+            // TODO: relics have a bad id system, need to fix that in the mean time we'll just use
+            // exisitng relics
+            int relicIndex = 1;
+            int relicEndIndex = 4;
+
+            for (AbstractRelic relic : AbstractDungeon.player.relics) {
+                if (relicIndex > relicEndIndex) {
+                    break;
+                }
+
+                JsonObject relicJson = new JsonObject();
+
+                // TODO: THIS THING SHOULD NOT BE HERE
+                relicJson.addProperty("id", relicIndex);
+
+                relicJson.addProperty("name", relic.name);
+                relicJson.addProperty("description", relic.description);
+                relicJson.addProperty("x_pos", relic.hb.x);
+                relicJson.addProperty("y_pos", relic.hb.y);
+
+                relicIndex++;
+                relicsArray.add(relicJson);
+            }
         }
 
         jsonBody.addProperty("player_current_hp", playerHp);
         jsonBody.addProperty("twitch_username", "twitchslayssspire");
         jsonBody.addProperty("player_max_hp", playerMaxHp);
+
+        jsonBody.add("relics", relicsArray);
 
         out.write(jsonBody.toString());
         out.close();
